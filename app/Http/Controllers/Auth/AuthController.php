6f1403a76360dc43db63c207c989fd6e82134f63
Komiya Tomoki <tomoki.komiya@epkotsoftware.co.jp>
2022-04-08 14:02:06 +0900

    FatControler対策

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -10,6 +10,9 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
+    public function __construct(User $user){
+    $this->user = $user;
+    }
     // @return View
     public function showLogin() {
         return view('login.login_form');
@@ -19,10 +22,9 @@ class AuthController extends Controller
         $credentials = $request->only('email','password',);
 
         // アカウントがロックされていたら弾く
-        $user = User::where('email', '=', $credentials['email'])->first();
-
+        $user = $this->user->getUserByEmail($credentials['email']);
         if (!is_null($user)) {
-            if ($user->locked_flg === 1) {
+            if ($this->user->isAccountLocked($user)) {
                 return back()->withErrors([
                     'danger' => 'アカウントがロックされています。',
                 ]);
@@ -30,20 +32,15 @@ class AuthController extends Controller
             if (Auth::attempt($credentials)) {
                 $request->session()->regenerate();
                 // ログインに成功したらエラーアカウントを0にする
-                if ($user->error_count > 0) {
-                    $user->error_count = 0;
-                    $user->save();
-                }
+                $this->user->resetErrorCount($user);
 
                 return redirect()->route('home')->with('success', 'ログイン成功しました！');
             }
 
             // ログインに失敗したらエラーカウントを1増やす
-            $user->error_count = $user->error_count +  1;
+            $user->error_count = $this->user->addErrorCount($user->error_count);
             // エラーアカウントが6以上の場合はアカウントをロックする
-            if ($user->error_count > 5) {
-                $user->locked_flg = 1;
-                $user->save();
+            if ($this->user->lockAccount($user)) {
                 return back()->withErrors([
                     'danger' => 'アカウントがロックされました。解除したい場合運営者に連絡してください',
                 ]);
