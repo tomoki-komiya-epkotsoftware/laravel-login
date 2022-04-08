21dac35bef7a22f1c19fcbc5c8a6c9beb18fa5ae
Komiya Tomoki <tomoki.komiya@epkotsoftware.co.jp>
2022-04-08 11:09:11 +0900

    create account lock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -17,15 +18,41 @@ class AuthController extends Controller
     public function login(LoginFormRequest $request) {
         $credentials = $request->only('email','password',);
 
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
+        // アカウントがロックされていたら弾く
+        $user = User::where('email', '=', $credentials['email'])->first();
 
-            return redirect()->route('home')->with('success', 'ログイン成功しました！');
+        if (!is_null($user)) {
+            if ($user->locked_flg === 1) {
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされています。',
+                ]);
+            }
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+                // ログインに成功したらエラーアカウントを0にする
+                if ($user->error_count > 0) {
+                    $user->error_count = 0;
+                    $user->save();
+                }
+
+                return redirect()->route('home')->with('success', 'ログイン成功しました！');
+            }
+
+            // ログインに失敗したらエラーカウントを1増やす
+            $user->error_count = $user->error_count +  1;
+            // エラーアカウントが6以上の場合はアカウントをロックする
+            if ($user->error_count > 5) {
+                $user->locked_flg = 1;
+                $user->save();
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされました。解除したい場合運営者に連絡してください',
+                ]);
+            }
+            $user->save();
         }
-
-        return back()->withErrors([
-            'danger' => 'メールアドレスかパスワードが間違っています。',
-        ]);
+            return back()->withErrors([
+                'danger' => 'メールアドレスかパスワードが間違っています。',
+            ]);
     }
     /**
      * ユーザーをアプリケーションからログアウトさせる
